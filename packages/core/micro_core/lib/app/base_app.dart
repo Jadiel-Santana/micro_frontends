60b4a9e8683eca034b7f237e022cd9373d92e162
Jadiel Santana <jadiel.bsantana@gmail.com>
2022-10-06 21:27:33 -0300

    🧩️: Create MicroCore package

---
+++ packages/core/micro_core/lib/app/base_app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+abstract class BaseApp {
+  List<MicroApp> get microApps;
+
+  Map<String, WidgetBuilderArgs> get baseRoutes;
+
+  final Map<String, WidgetBuilderArgs> routes = {};
+
+  void registerRouters() {
+    if (baseRoutes.isNotEmpty) {
+      routes.addAll(baseRoutes);
+    }
+    if (microApps.isNotEmpty) {
+      for (MicroApp microApp in microApps) {
+        routes.addAll(microApp.routes);
+      }
+    }
+  }
+
+  void registerInjections() {
+    if (microApps.isNotEmpty) {
+      for (MicroApp microApp in microApps) {
+        microApp.injectionsRegister();
+      }
+    }
+  }
+
+  Route<dynamic>? generateRoute(RouteSettings settings) {
+    final routerName = settings.name;
+    final routerArgs = settings.arguments;
+
+    final navigateTo = routes[routerName];
+    if (navigateTo == null) {
+      return null;
+    }
+    return MaterialPageRoute(
+      builder: (context) => navigateTo.call (context, routerArgs),
+    );
+  }
+}
---
+++ packages/core/micro_core/lib/app/micro_core_utils.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+typedef WidgetBuilderArgs = Widget Function(BuildContext context, Object? args);
+GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+//late GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
